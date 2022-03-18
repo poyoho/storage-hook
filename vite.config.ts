dc0f26360872ef869b31292313ae7d8b9ca9fa80
yoho <panyoho@gmail.com>
2022-03-18 14:25:15 +0800

    chore: codecov

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   test: {
     environment: 'happy-dom',
     coverage: {
-      reporter: ['clover']
+      reporter: ['json', "text", "text-summary"]
     }
   }
 })
